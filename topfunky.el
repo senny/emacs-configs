738069194ec820f3478195dc2f46c58931159314
Geoffrey Grosenbach <boss@topfunky.com>
2009-03-18 13:20:18 -0700

    Activated rinari and added some keyboard shortcuts

--- topfunky.el
+++ topfunky.el
@@ -47,8 +47,13 @@
 ;; TODO javascript-indent-level 2
 
 ;; Rinari
-;; (add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
-;; (require 'rinari)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/jump.el"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
+(require 'rinari)
+(define-key rinari-minor-mode-map [(control meta shift down)] 'rinari-find-rspec)
+(define-key rinari-minor-mode-map [(control meta shift left)] 'rinari-find-controller)
+(define-key rinari-minor-mode-map [(control meta shift up)] 'rinari-find-model)
+(define-key rinari-minor-mode-map [(control meta shift right)] 'rinari-find-view)
 
 (autoload 'applescript-mode "applescript-mode" "major mode for editing AppleScript source." t)
 (setq auto-mode-alist
