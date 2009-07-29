da1f23531d3308199f601d68eeacd4f34b32423e
Yves Senn <yves.senn@gmail.com>
2009-07-30 00:13:24 +0200

    added rhtml-mode for ruby erb files

--- private/color-theme-twilight.el
+++ private/color-theme-twilight.el
@@ -31,10 +31,11 @@
       (mouse-color . "sienna1"))
      (border-glyph ((t (nil))))
      (buffers-tab ((t (:background "black" :foreground "white"))))
-     
+
      (preprocessor ((t (:foreground "Aquamarine"))))
      (highlighted ((t (:background "#606060"))))
      (highlighted-line ((t (:background "#101010"))))
+     (important-region ((t (:background "#000000"))))
      (flashy-highlight ((t (:background "#6C6A41"))))
      (default ((t (:foreground "#F8F8F8"))))
      (variable ((t (:foreground "#7587A6"))))
@@ -50,7 +51,7 @@
 
      (paren-face ((t (:inherit default))))
      (esk-paren-face ((t (:inerhit default))))
-     
+
      (font-lock-builtin-face ((t (:inhert default))))
      (font-lock-comment-face ((t (:inherit comment))))
      (font-lock-constant-face ((t (:inherit constant))))
@@ -68,7 +69,7 @@
      (font-lock-type-face ((t (:inherit type))))
      (font-lock-variable-name-face ((t (:inherit variable))))
      (font-lock-warning-face ((t (:bold t :foreground "Pink"))))
-     
+
      (jde-java-font-lock-doc-tag-face ((t (:inherit doc :bold t))))
      (jde-java-font-lock-constant-face ((t (:inherit constant))))
      (jde-java-font-lock-link-face ((t (:underline t :foreground "dark-blue"))))
@@ -79,13 +80,13 @@
      (jde-db-spec-breakpoint-face ((t (:inherit diff-header))))
      (jde-db-requested-breakpoint-face ((t (:inherit diff-changed))))
      (jde-db-active-breakpoint-face ((t (:inherit diff-added))))
-     
+
      (ecb-default-highlight-face ((t (:inherit highlighted))))
-     
+
      (log4j-font-lock-info-face ((t (:inherit variable))))
      (log4j-font-lock-error-face ((t (:inherit constant))))
      (log4j-font-lock-warn-face ((t (:inherit keyword))))
-     
+
      (diff-header ((t (:inherit default :background "#0E2231"))))
      (diff-added ((t (:inherit default :background "#253B22"))))
      (diff-removed ((t (:inherit default :background "#420E09"))))
@@ -100,7 +101,7 @@
 
      (flymake-errline ((t (:inherit code-error))))
      (flymake-warnline ((t (:inherit code-warning))))
-     
+
      (ediff-current-diff-A ((((class color) (min-colors 16)) (:background "#01243C" :foreground "white"))))
      (ediff-current-diff-Ancestor ((((class color) (min-colors 16)) (:background "#4D0600" :foreground "white"))))
      (ediff-current-diff-B ((((class color) (min-colors 16)) (:background "#574A00" :foreground "white"))))
@@ -116,7 +117,7 @@
      (ediff-odd-diff-A ((((class color) (min-colors 16)) (:background "#222222"))))
      (ediff-odd-diff-Ancestor ((((class color) (min-colors 16)) (:background "#222222"))))
      (ediff-odd-diff-B ((((class color) (min-colors 16)) (:background "#222222"))))
- 
+
      (js2-error-face ((t (:inherit code-error))))
      (js2-external-variable-face ((t (:foreground))))
      (js2-function-param-face ((t (:inherit variable))))
@@ -124,11 +125,11 @@
      (js2-private-function-call-face ((t (:inherit default))))
      (js2-private-member-face ((t (:inherit variable))))
      (js2-warning-face ((t (:inherit code-warning))))
-;;                                         (js2-jsdoc-html-tag-delimiter-face
-;;                                         (js2-jsdoc-html-tag-name-face       
-;;                                         (js2-jsdoc-tag-face                 
-;;                                         (js2-jsdoc-type-face                
-;;                                         (js2-jsdoc-value-face
+     ;;                                         (js2-jsdoc-html-tag-delimiter-face
+     ;;                                         (js2-jsdoc-html-tag-name-face
+     ;;                                         (js2-jsdoc-tag-face
+     ;;                                         (js2-jsdoc-type-face
+     ;;                                         (js2-jsdoc-value-face
 
      (html-tag-face ((t (:inherit keyword))))
 
@@ -156,7 +157,7 @@
 
      (sgml-namespace ((t (:inherit type))))
      (css-selector ((t (:inherit keyword))))
-     
+
      (yas/mirror-highlight-face ((t (:background "#4E404F"))))
      (yas/field-highlight-face ((t (:background "#2E2E2E"))))
 
@@ -171,12 +172,19 @@
      (newsticker-treeview-selection-face ((t (:inherit highlighted))))
      (newsticker-treeview-face ((t (:inherit default))))
      (newsticker-treeview-immortal-face ((t (:inherit keyword))))
-     
+
      (ido-only-match ((t (:inherit string))))
      (ido-subdir ((t (:inherit constant))))
-     
+
      (mac-ts-caret-position ((t (:inherit highlighted-line))))
      
+     (erb-comment-face ((t (:inherit comment))))
+     (erb-comment-delim-face ((t (:inherit comment))))
+     (erb-exec-face ((t (:inherit important-region))))
+     (erb-exec-delim-face ((t (:inherit preprocessor))))
+     (erb-out-face ((t (:inherit important-region))))
+     (erb-out-delim-face ((t (:inherit preprocessor))))
+
      (gui-element ((t (:background "#D4D0C8" :foreground "black"))))
      (region ((t (:background "#333333"))))
      (minibuffer-prompt ((t (:background "#141414" :foreground "cyan"))))
