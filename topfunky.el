c34fb216a0583b3c77a80bfbc2ce2b6f73f4de1d
Geoffrey Grosenbach <boss@topfunky.com>
2009-09-23 13:31:40 -0700

    Use tab for autocomplete and yasnippet. From http://github.com/soro/emacs-starter-kit/commits/master

--- topfunky.el
+++ topfunky.el
@@ -66,6 +66,35 @@
 ;; Javascript
 ;; TODO javascript-indent-level 2
 
+;; Remove scrollbars and make hippie expand
+;; work nicely with yasnippet
+(scroll-bar-mode -1)
+(require 'hippie-exp)
+(setq hippie-expand-try-functions-list
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name
+        try-complete-file-name-partially
+        try-complete-lisp-symbol
+        try-complete-lisp-symbol-partially
+        try-expand-line
+        try-expand-line-all-buffers
+        try-expand-list
+        try-expand-list-all-buffers
+        try-expand-whole-kill))
+
+(defun indent-or-complete ()
+  (interactive)
+  (if (and (looking-at "$") (not (looking-back "^\\s-*")))
+      (hippie-expand nil)
+      (indent-for-tab-command)))
+(add-hook 'find-file-hooks (function (lambda ()
+  (local-set-key (kbd "TAB") 'indent-or-complete))))
+
+
 ;; Rinari
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/jump.el"))
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/rinari"))
