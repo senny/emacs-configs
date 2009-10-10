d9570e0018a4615d566f265a06a26a1f565fb5fe
Yves Senn <yves.senn@gmail.com>
2009-10-10 18:28:15 +0200

    fixed cocoa emacs

---
+++ private/custom/cocoa-emacs-custom.el
@@ -0,0 +1,19 @@
+;; customization
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(emulate-mac-swiss-german-keyboard-mode t)
+ '(inhibit-startup-screen t)
+ '(jde-jdk (quote ("CurrentJDK OS-X")))
+ '(jde-jdk-registry (quote (("1.5" . "/System/Library/Frameworks/JavaVM.framework/Versions/1.5") ("1.6" . "/System/Library/Frameworks/JavaVM.framework/Versions/1.6") ("CurrentJDK OS-X" . "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK"))))
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(visible-bell nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
