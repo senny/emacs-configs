2b76314088ea7d4ecae8fc3603f7bfece8db8d6e
Yves Senn <yves.senn@gmail.com>
2009-10-10 20:31:00 +0200

    open files in the same frame

--- private/display.el
+++ private/display.el
@@ -7,6 +7,9 @@
 (vendor 'maxframe)
 (add-hook 'window-setup-hook 'maximize-frame t)
 
+;; open new files in the same window
+(setq gnuserv-frame (car (frame-list)))
+
 ;; Package to interactivly resize split screens
 ;; (require 'winsize)
 
