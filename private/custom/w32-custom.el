4c0429fa446bf1f04f292f87f96614008f9540ae
Yves Senn <yves.senn@gmail.com>
2009-10-21 17:14:25 +0200

    set firefox to the default browser

--- private/custom/w32-custom.el
+++ private/custom/w32-custom.el
@@ -4,6 +4,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-firefox))
  '(clearcase-diff-on-checkin t)
  '(clearcase-suppress-checkout-comments t)
  '(compilation-scroll-output t)
