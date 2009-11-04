818621e0793a97ce9f2e304a2a57fd5afb2241b7
Geoffrey Grosenbach <boss@topfunky.com>
2009-11-03 17:00:58 -0800

    Mac Pro-specific font settings

--- topfunky.el
+++ topfunky.el
@@ -166,9 +166,6 @@
 ;; Mercurial
 ;;(require 'mercurial)
 
-;; Font
-(set-face-font 'default "-apple-inconsolata-medium-r-normal--20-0-72-72-m-0-iso10646-1")
-
 ;; Color Themes
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme"))
 (require 'color-theme)
