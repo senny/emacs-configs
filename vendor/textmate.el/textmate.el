5079c4e4a52f0fd65eccf23e478d52568f34e65e
Geoffrey Grosenbach <boss@topfunky.com>
2009-12-15 17:15:56 -0800

    Ignore .git directories when constructing textmate-style file menu

--- vendor/textmate.el/textmate.el
+++ vendor/textmate.el/textmate.el
@@ -74,7 +74,7 @@
 (defvar *textmate-project-root* nil)
 (defvar *textmate-project-files* '())
 (defvar *textmate-gf-exclude*
-  "/\\.vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|UnitTesting")
+  "/\\.git|script|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|UnitTesting")
 
 ;;; Bindings
 
