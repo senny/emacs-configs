50d3b931d022397a4c7b736f3384a1aedc515ea6
Geoffrey Grosenbach <boss@topfunky.com>
2009-12-23 09:35:12 -0800

    Experimenting with Gary Bernhardt's cyclomatic complexity script

--- topfunky.el
+++ topfunky.el
@@ -80,6 +80,17 @@
 
 ;; Major Modes
 
+;; Python
+;; Live cyclomatic complexity script from @garybernhardt
+;; http://blog.extracheese.org/2009/11/refactoring_a_cyclomatic_complexity_script.html
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/pycomplexity"))
+(require 'linum)
+(require 'pycomplexity)
+(add-hook 'python-mode-hook
+          (function (lambda ()
+                      (pycomplexity-mode)
+                      (linum-mode))))
+
 ;; Javascript
 (setq js2-basic-offset 2)
 (setq js2-auto-indent-flag nil)
