3302756f0a01ee6ee24a645a04087b853afebdbb
Geoffrey Grosenbach <boss@topfunky.com>
2010-01-23 17:17:51 -0800

    More org-mode overrides for my personal buffer switching key combos

--- topfunky.el
+++ topfunky.el
@@ -176,6 +176,7 @@
             (local-set-key [(control meta return)] 'org-insert-heading)
             (local-set-key [(control shift left)] 'previous-buffer)
             (local-set-key [(control shift right)] 'next-buffer)
+            (local-set-key [(meta shift right)] 'ido-switch-buffer)
             ))
 
 
