0b9b5f45df7bad78137f480c8067d3c9ae6aee0d
Yves Senn <yves.senn@gmail.com>
2010-02-22 19:11:50 +0100

    removed unused complexity packages

--- vendor/pycomplexity/tests/manual/test_update_function.py
+++
@@ -1,9 +0,0 @@
-def foo():
-    x if x else x if x else x if x else x if x else x if x else x if x else x
-    # Delete this line, then save. The function should go from yellow to
-    # green. Then undo and save. The *whole thing* should go back to yellow.
-    x if x else x
-    pass
-    pass
-    pass
-
