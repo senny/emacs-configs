d6b782a7d0d6ec4348460b65355d3bb6fc7b127e
Yves Senn <yves.senn@gmail.com>
2010-06-14 16:50:05 +0200

    updated language files

--- private/language-erlang.el
+++ private/language-erlang.el
@@ -1,11 +1,14 @@
+;; setup Erlang environment
 (setq load-path (cons  "/opt/homebrew/lib/erlang/lib/tools-2.6.5.1/emacs"
                        load-path))
 (setq erlang-root-dir "/opt/homebrew/lib/erlang")
 (setq exec-path (cons "/opt/homebrew/lib/erlang/bin" exec-path))
 
-(add-hook 'erlang-mode-hook
-          (lambda ()
-            (define-key erlang-mode-map (kbd "C-c C-l") 'erlang-compile)
-            ))
+;; customizations
+(eval-after-load 'erlang
+  (add-hook 'erlang-mode-hook
+            (lambda ()
+              (define-key erlang-mode-map (kbd "C-c C-l") 'erlang-compile)
+              )))
 
 (require 'erlang-start)
--- private/language-java.el
+++ private/language-java.el
@@ -1,16 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.jsp$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.java$" . java-mode))
 
-(eval-after-load 'java-mode-hook
-  '(progn
-     ;;;; Additional Libraries
-     (require 'javadoc-help)
-     (require 'java-mode-indent-annotations)
-     
-     ;;;; Bindings
-     (global-set-key (kbd "C-h r") 'yari)
-     ))
-
-
-
-(provide 'language-java)
+(add-hook 'java-mode-hook
+          (lambda ()
+            ;; Additional Libraries
+            (require 'javadoc-help)
+            (require 'java-mode-indent-annotations)
+            ))
