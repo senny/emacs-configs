d91a0d787c6737d3c5dadf2607de7a686e5107bf
Yves Senn <yves.senn@gmail.com>
2010-06-16 15:42:53 +0200

    config cleanup

--- private/modes.el
+++ private/modes.el
@@ -29,8 +29,6 @@
 (global-set-key "\C-ck" 'mode-compile-kill)
 
 ;; Make emacs act like textmate
-(vendor 'textmate)
-(textmate-mode 1)
 (eval-after-load 'textmate
   '(progn
      (global-unset-key (kbd "M-t"))
@@ -39,6 +37,9 @@
      (define-key *textmate-mode-map* (kbd "M-t") 'textmate-goto-file)
      (define-key *textmate-mode-map* (kbd "M-w") 'textmate-goto-symbol)
      (define-key *textmate-mode-map* (kbd "M-T") 'textmate-goto-symbol)))
+
+(vendor 'textmate)
+(textmate-mode 1)
 
 ;; whitespace mode
 (vendor 'whitespace)
