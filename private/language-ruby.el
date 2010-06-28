0cf60a04d42a12a1c75b334a7d053d9562363e51
Yves Senn <yves.senn@gmail.com>
2010-06-28 15:01:21 +0200

    moved senny-open-gem into the rvm.el project

--- private/language-ruby.el
+++ private/language-ruby.el
@@ -9,13 +9,6 @@
     (ruby-compilation-this-buffer)
     (pop-to-buffer origin)))
 
-(defun senny-ruby-open-gem ()
-  (interactive)
-  (let ((gem-dir "/Library/Ruby/Gems/1.8/gems/"))
-    (find-file (ido-open-find-directory-files
-                (concat gem-dir (ido-completing-read "Gem: "
-                                                     (directory-files gem-dir nil "^[^.]")))))))
-
 (eval-after-load 'ruby-mode
   '(progn
      ;;;; Additional Libraries
@@ -28,7 +21,7 @@
      ;;;; Bindings
      (global-set-key (kbd "C-h r") 'yari)
      (define-key ruby-mode-map (kbd "C-M-r") 'senny-ruby-compilation-this-buffer)
-     (define-key ruby-mode-map (kbd "C-c C-r g") 'senny-ruby-open-gem)
+     (define-key ruby-mode-map (kbd "C-c C-r g") 'rvm-open-gem)
   ))
 
 ;;;; Ruby Block Mode
