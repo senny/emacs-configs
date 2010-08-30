f6ff65cc131aabacd1527c05558df9f900a6b3c3
Yves Senn <yves.senn@gmail.com>
2010-08-30 08:00:30 +0200

    . fixed cucumber regexp highlighting

--- private/language-ruby.el
+++ private/language-ruby.el
@@ -18,9 +18,19 @@
      ;; active the default ruby configured with rvm
      (when (fboundp 'rvm-use-default)
        (rvm-use-default))
-     
+
      ;;;; Bindings
      (global-set-key (kbd "C-h r") 'yari)
      (define-key ruby-mode-map (kbd "C-M-r") 'senny-ruby-compilation-this-buffer)
      (define-key ruby-mode-map (kbd "C-c C-r g") 'rvm-open-gem)
-  ))
+
+     ;; fix syntax highlighting for Cucumber Step Definition regexps
+     (add-to-list 'ruby-font-lock-syntactic-keywords '("\\(\\(\\)\\(\\)\\|Given\\|When\\|Then\\)\\s *\\(/\\)[^/\n\\\\]*\\(\\\\.[^/\n\\\\]*\\)*\\(/\\)"
+                                                       (4 (7 . ?/))
+                                                       (6 (7 . ?/))))
+     ))
+
+
+;;   (add-to-list 'ruby-font-lock-syntactic-keywords '("\\(^\\|[=(,~?:;<>]\\|\\(^\\|\\s \\)\\(if\\|elsif\\|unless\\|while\\|until\\|when\\|and\\|or\\|&&\\|||\\)\\|g?sub!?\\|scan\\|split!?\\)\\s *\\(/\\)[^/\n\\\\]*\\(\\\\.[^/\n\\\\]*\\)*\\(/\\)"
+;; (4 (7 . ?/))
+;; (6 (7 . ?/))))
