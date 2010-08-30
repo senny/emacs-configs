5790a716e385675dc28dc2d5d5ea77a9197b7174
Yves Senn <yves.senn@gmail.com>
2010-08-30 19:24:02 +0200

    . moved perspective.el config into own configuration file

--- private/display.el
+++ private/display.el
@@ -34,34 +34,3 @@
   (color-theme-initialize))
 (setq color-theme-is-global t)
 (color-theme-twilight)
-
-;; (require 'winring)
-;; (winring-initialize)
-
-;;;; Perspective
-(add-to-list 'load-path (concat dotfiles-dir "/vendor/perspective.el"))
-(require 'perspective)
-(persp-mode)
-
-(defmacro senny-persp (name &rest body)
-  `(let ((initialize (not (gethash ,name perspectives-hash))))
-     (persp-switch ,name)
-     (when initialize ,@body)))
-
-(defun senny-persp/jabber ()
-  (interactive)
-  (senny-persp "Jabber"
-               (jabber-connect-all)
-               (call-interactively 'jabber-display-roster)
-               (switch-to-buffer jabber-roster-buffer)))
-
-(defun senny-persp/irc ()
-  (interactive)
-  (senny-persp "IRC"
-               (erc)
-               (dolist (channel '("emacs" "ruby" "cucumber"))
-                 (erc-join-channel channel))))
-
-(defun senny-persp/main ()
-  (interactive)
-  (senny-persp "main"))
---
+++ private/perspectives.el
@@ -0,0 +1,27 @@
+;;;; Perspective
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/perspective.el"))
+(require 'perspective)
+(persp-mode)
+
+(defmacro senny-persp (name &rest body)
+  `(let ((initialize (not (gethash ,name perspectives-hash))))
+     (persp-switch ,name)
+     (when initialize ,@body)))
+
+(defun senny-persp/jabber ()
+  (interactive)
+  (senny-persp "Jabber"
+               (jabber-connect-all)
+               (call-interactively 'jabber-display-roster)
+               (switch-to-buffer jabber-roster-buffer)))
+
+(defun senny-persp/irc ()
+  (interactive)
+  (senny-persp "IRC"
+               (erc)
+               (dolist (channel '("emacs" "ruby" "cucumber"))
+                 (erc-join-channel channel))))
+
+(defun senny-persp/main ()
+  (interactive)
+  (senny-persp "main"))
