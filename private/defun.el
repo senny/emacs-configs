ab540a601f06cd96161a966742b4499d80550920
Yves Senn <yves.senn@gmail.com>
2010-09-26 17:56:58 +0200

    . color-theme refactoring
    allows me to keep multiple color themes up to date when customizing new faces

--- private/defun.el
+++ private/defun.el
@@ -404,7 +404,7 @@ major mode for the newly created buffer."
         (generated-autoload-file autoload-file))
     (when (or force-regen
               (not (file-exists-p autoload-file))
-              (some (lambda (f) (file-newer-than-file-p f autoload-file))
+              (some (lambda (f) (and (not (file-directory-p f)) (file-newer-than-file-p f autoload-file)))
                     (directory-files autoload-dir t "\\.el$")))
       (message "Updating autoloads...")
       (let (emacs-lisp-mode-hook)
