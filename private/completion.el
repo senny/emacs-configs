0906890ced0ecf415279f1593d3633d29d9e0304
Yves Senn <yves.senn@gmail.com>
2010-10-05 07:41:28 +0200

    fixed completion bug

--- private/completion.el
+++ private/completion.el
@@ -4,12 +4,13 @@
 (setq yas/root-directory (concat private-config-dir "/snippets"))
 (setq yas/prompt-functions '(yas/dropdown-prompt yas/ido-prompt))
 (yas/load-directory yas/root-directory)
-(setq yas/fallback-behavior '(apply auto-complete))
+(set-default 'yas/fallback-behavior '(apply auto-complete))
 ;; (yas/initialize)
 
 ;;;; auto-complete
 (vendor 'auto-complete)
-(add-to-list 'ac-dictionary-directories (concat vendor-dir "/auto-complete/dict"))
+(add-to-list 'ac-dictionary-directories
+             (concat vendor-dir "/auto-complete/dict"))
 (require 'auto-complete-config)
 (ac-config-default)
 (setq ac-auto-start nil
