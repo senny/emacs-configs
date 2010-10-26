c090eaf9902f460cced12f95cfdb8665f5e9e80d
Yves Senn <yves.senn@gmail.com>
2010-10-26 15:47:01 +0200

    add pairs to sql-mode

--- private/languages/sql.el
+++ private/languages/sql.el
@@ -1,5 +1,6 @@
 (defun default-sql-mode-hook ()
   (setq tab-width 4)
+  (set-pairs '("(" "{" "[" "\"" "\'"))
   (sql-set-product 'postgres)
   (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-yasnippet))
   (auto-complete-mode t))
