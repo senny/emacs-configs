9a9460a97070f61927d205bf7649bad9e4c91c18
Yves Senn <yves.senn@gmail.com>
2010-10-26 15:12:57 +0200

    sql improvements + use postgres sql as default product

--- private/languages/sql.el
+++ private/languages/sql.el
@@ -1,7 +1,13 @@
 (defun default-sql-mode-hook ()
   (setq tab-width 4)
-  (sql-set-product 'mysql)
+  (sql-set-product 'postgres)
   (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-yasnippet))
   (auto-complete-mode t))
+
+(eval-after-load 'sql
+  '(progn
+     (define-key sql-mode-map (kbd "C-c M-r") 'sql-send-region)
+     (define-key sql-mode-map (kbd "C-c v") 'sql-send-buffer)
+     ))
 
 (add-hook 'sql-mode-hook 'default-sql-mode-hook)
