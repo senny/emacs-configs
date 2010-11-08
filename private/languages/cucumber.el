a826eaf6dc9c51f71f778362e9c12d06ec66e58f
Yves Senn <yves.senn@gmail.com>
2010-11-08 15:33:19 +0100

    . submodule update

--- private/languages/cucumber.el
+++ private/languages/cucumber.el
@@ -1,3 +1,10 @@
 ;;;; Cucumber
 (add-to-list 'load-path (concat vendor-dir "/cucumber.el"))
 (require 'feature-mode)
+
+
+;; Hooks
+(defun default-cucumber-hook ()
+  (set-pairs '("(" "\"" "\'")))
+
+(add-hook 'feature-mode-hook 'default-cucumber-hook)
