b6b3c82e62f6079f2a432547cd5147d03dac9059
Yves Senn <yves.senn@garaio.com>
2011-07-08 13:10:01 +0200

    backups go into the backup folder

--- private/backup.el
+++ private/backup.el
@@ -4,10 +4,9 @@
 
 (make-directory user-temporary-file-directory t)
 (setq backup-by-copying t)
-(setq backup-directory-alist
-      `(("." . ,user-temporary-file-directory)
-        (,tramp-file-name-regexp nil)))
 (setq auto-save-list-file-prefix
       (concat user-temporary-file-directory ".auto-saves-"))
+(setq backup-directory-alist
+      `((".*" . ,user-temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,user-temporary-file-directory t)))
